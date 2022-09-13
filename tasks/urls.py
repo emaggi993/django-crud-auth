39e85501ac7c35b8add0c4a3cd9217e73a18e16c
emaggi <esrma3010@gmail.com>
2022-09-13 07:51:48 -0400

    20220913 login y register correcto

---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('', home, name='home'),
+    path('tasks/', tasks, name='tasks'),
+    path('logout/', signout, name='logout'),
+    path('signin/', signin, name='signin'),
+]
